54db60c11f1c3d3535dcc4e3ac9c88a9bf43150d
Donald Trump <viniciuslara018@gmail.com>
2024-02-01 15:11:58 -0300

    ACABOOOOO

---
+++ F16.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite um numero: "))
+
+media = (num1 + num2) / 2
+
+if media >= 7:
+    print('aprovado seu imbecil')
+else:
+    print("MUITO BURRO, VOLTA A VER SKIBIDI TOILET")
